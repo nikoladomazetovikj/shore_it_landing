14ea824085ce87dcfc94dfc11ff2681563f2c551
nikoladomazetovikj <nikolad150@gmail.com>
2024-06-07 22:08:02 +0200

    improvements

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { resolve } from 'path';
 export default {
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                en: resolve(__dirname, 'src/en/index.html')
+            }
+        }
     },
     server: {
         port: 8080
